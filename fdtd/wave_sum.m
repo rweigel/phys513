9fcb7214dbe362200f9433b73c4085e6db49f4f6
Bob Weigel <rweigel@gmu.edu>
2022-11-13 10:28:51 -0500

    HW 10

--- fdtd/wave_sum.m
+++ fdtd/wave_sum.m
@@ -1,9 +1,12 @@
 Nx = 300;
 x = [1:Nx];
 lambda = 100;
-rho = 0.5;
+rho = 1;
+
 
 if 1
+vidfile = VideoWriter('wave_sum_1_rho_1.0.mp4','MPEG-4');
+open(vidfile);
 figure(1);clf
 for i = 1:300
     Ei = cos(2*pi*(x-i)/lambda);
@@ -15,16 +18,16 @@ for i = 1:300
     % Keep past time steps
     hold on;
     grid on;
-    plot(Ei,'k','LineWidth',1,'Color',[1,1,1,0.4]/2);
-    plot(Er,'k','LineWidth',1,'Color',[1,1,1,0.4]/2);    
+    plot(Ei,'r','LineWidth',1,'Color',[1,1,1,0.4]/2);
+    plot(Er,'b','LineWidth',1,'Color',[1,1,1,0.4]/2);    
     if i > 1
         % Delete previous current time step thick black line
         delete(h1)
         delete(h2)
     end
     % Plot current time step as thick black line
-    h1 = plot(Ei,'k','LineWidth',2);
-    h2 = plot(Er,'k','LineWidth',2);
+    h1 = plot(Ei,'r','LineWidth',2);
+    h2 = plot(Er,'b','LineWidth',2);
     
     set(gca,'Ylim',[-2,2]);
     set(gca,'Xlim',[1,Nx]);
@@ -33,13 +36,19 @@ for i = 1:300
     %hold off;
     if mod(i,100) == 0
         % Allow early termination of animation
-        input('Continue?');
+        %input('Continue?');
     end
     drawnow;
+    frame = getframe(gcf); 
+    writeVideo(vidfile,frame);    
+
 end
+close(vidfile)
 end
 
 if 0
+vidfile = VideoWriter('wave_sum_2_rho_1.mp4','MPEG-4');
+open(vidfile);
 figure(1);clf
 for i = 1:500
     Ei = cos(2*pi*(x-i)/lambda);
@@ -56,9 +65,13 @@ for i = 1:500
     if i > 1
         % Delete previous current time step thick black line
         delete(h1)
+        delete(h2)
+        delete(h3)
     end
     % Plot current time step as thick black line
-    h1 = plot(Et,'k','LineWidth',2);
+    h1 = plot(Ei,'r','LineWidth',2);
+    h2 = plot(Er,'b','LineWidth',2);
+    h3 = plot(Et,'k','LineWidth',2);
     
     set(gca,'Ylim',[-2,2]);
     set(gca,'Xlim',[1,Nx]);
@@ -67,8 +80,11 @@ for i = 1:500
     %hold off;
     if mod(i,100) == 0
         % Allow early termination of animation
-        input('Continue?');
+        %input('Continue?');
     end
     drawnow;
+    frame = getframe(gcf); 
+    writeVideo(vidfile,frame);    
 end
+close(vidfile)
 end

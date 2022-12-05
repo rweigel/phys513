d003ae60bfe84618883eb1a1f891581c267d185e
Bob Weigel <rweigel@gmu.edu>
2022-12-05 11:53:53 -0500

    HW11 solns

--- hw/HW11.m
+++ hw/HW11.m
@@ -1,8 +1,8 @@
 clear
 
 Vs = 1;
-rho1 = -1/7;
-rho2 = -1/5;
+rho1 = -1/7; % Not actually rho1; see notes.
+rho2 = -1/5; % Not actually rho2; see notes.
 lambda1 = 5e-3;
 lambda2 = 10e-3;
 
@@ -16,16 +16,20 @@ V1p = Vs/(exp(1j*beta1*(l1+l2)) + rho1*exp(1j*beta1*(l1+l2)));
 V2p = V1p*(exp(1j*beta1*l2) + rho1*exp(1j*beta1*l2)) / ...
           (exp(1j*beta2*l2) + rho2*exp(1j*beta2*l2));
 
-x1 = [-15:0.1:-10]*1e-3;
+x1 = [-(l1+l2):0.1e-3:-l1];
 V1 = V1p*(exp(-1j*beta1*x1) + rho1*exp(1j*beta1*x1));
-x2 = [-10:0.1:0]*1e-3;
+x2 = [-l1:0.1e-3:0];
 V2 = V2p*(exp(-1j*beta2*x2) + rho2*exp(1j*beta2*x2));
 
 clf;hold on; grid on;
-plot(x1, abs(V1),'k','LineWidth',2)
-plot(x2, abs(V2),'k','LineWidth',2)
+plot(x1/1e-3, abs(V1),'k','LineWidth',2)
+plot(x2/1e-3, abs(V2),'k','LineWidth',2)
 legend('|V(x)/V_s|')
 xlabel('x [cm]')
+
+VSWR1 = (1+abs(rho1))/(1-abs(rho1));
+VSWR2 = (1+abs(rho2))/(1-abs(rho2));
+title(sprintf('VSWR_1 = %.2f; VSWR_2 = %.2f',VSWR1,VSWR2));
 
 print('HW11.svg', '-dsvg');
 print('HW11.png', '-dpng', '-r300'); 

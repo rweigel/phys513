02bd979b22bff6d76aa6d1ab064dd3231644e825
Bob Weigel <rweigel@gmu.edu>
2022-12-05 12:38:17 -0500

    HW11 solns

--- hw/HW11.m
+++ hw/HW11.m
@@ -1,8 +1,6 @@
 clear
 
 Vs = 1;
-rho1 = -1/7; % Not actually rho1; see notes.
-rho2 = -1/5; % Not actually rho2; see notes.
 lambda1 = 5e-3;
 lambda2 = 10e-3;
 
@@ -12,14 +10,23 @@ l2 = (3/4)*lambda2;
 beta1 = 2*pi/lambda1;
 beta2 = 2*pi/lambda2;
 
-V1p = Vs/(exp(1j*beta1*(l1+l2)) + rho1*exp(1j*beta1*(l1+l2)));
-V2p = V1p*(exp(1j*beta1*l2) + rho1*exp(1j*beta1*l2)) / ...
-          (exp(1j*beta2*l2) + rho2*exp(1j*beta2*l2));
+rho1 = -1/7*exp(2j*beta1*l2);
+rho2 = -1/5;
 
-x1 = [-(l1+l2):0.1e-3:-l1];
+V1p = Vs/(exp(1j*beta1*(l1+l2)) + rho1*exp(-1j*beta1*(l1+l2)));
+V2p = V1p*(exp(1j*beta1*l2) + rho1*exp(-1j*beta1*l2)) / ...
+          (exp(1j*beta2*l2) + rho2*exp(-1j*beta2*l2));
+
+%lambda3 not given, so can't plot.
+%V3p = V2p*(1+rho2);
+
+x1 = [-(l1+l2):0.1e-3:-l2];
 V1 = V1p*(exp(-1j*beta1*x1) + rho1*exp(1j*beta1*x1));
-x2 = [-l1:0.1e-3:0];
+x2 = [-l2:0.1e-3:0];
 V2 = V2p*(exp(-1j*beta2*x2) + rho2*exp(1j*beta2*x2));
+
+%x3 = [0:0.1e-3:l1];
+%V3 = V3p*exp(-1j*beta3*x3);
 
 clf;hold on; grid on;
 plot(x1/1e-3, abs(V1),'k','LineWidth',2)
